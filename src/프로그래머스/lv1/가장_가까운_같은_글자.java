ce036638218fce627e8a372e40f5aa08fcc18e87
doHoaSen <applesusu999@gmail.com>
2025-11-22 22:03:55 +0900

    251122 프로그래머스 lv1: 3문제

---
+++ src/프로그래머스/lv1/가장_가까운_같은_글자.java
@@ -0,0 +1,33 @@
+package 프로그래머스.lv1;
+import java.util.*;
+
+public class 가장_가까운_같은_글자 {
+    class Solution {
+        public int[] solution(String s) {
+            int[] answer = new int[s.length()];
+            for(int i = 0; i < s.length(); i++){
+                // 인덱스 검사
+                int idx = s.substring(0, i).lastIndexOf(s.charAt(i));
+                if (idx == -1) answer[i] = -1;
+                else {
+                    answer[i] = i - idx;
+                }
+            }
+            return answer;
+        }
+    }
+
+    class Solution2 {
+        public int[] solution(String s) {
+            int[] answer = new int[s.length()];
+            HashMap<Character, Integer> map = new HashMap<>();
+
+            for(int i = 0; i < s.length(); i++){
+                char c = s.charAt(i);
+                answer[i] = i - map.getOrDefault(c, i+1);
+                map.put(c, i);
+            }
+            return answer;
+        }
+    }
+}
---
+++ src/프로그래머스/lv1/최소직사각형.java
@@ -0,0 +1,24 @@
+package 프로그래머스.lv1;
+
+import java.util.*;
+public class 최소직사각형 {
+    class Solution {
+        public int solution(int[][] sizes) {
+            int answer = 0;
+            // 그리디?
+            // 회전 가능 -> 정렬 후 1열의 최댓값 * 2열의 최댓값
+            int maxW = 0;
+            int maxH = 0;
+
+            for(int[] card: sizes){
+                int w = Math.max(card[0], card[1]);
+                int h = Math.min(card[0], card[1]);
+
+                maxW = Math.max(maxW, w);
+                maxH = Math.max(maxH, h);
+            }
+
+            return maxW * maxH;
+        }
+    }
+}
