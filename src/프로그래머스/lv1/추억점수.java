04167d366ca15ec6bcc28afa5bbed33c81fc6feb
doHoaSen <applesusu999@gmail.com>
2025-11-27 17:03:02 +0900

    251127 프로그래머스 lv1: 5문제

---
+++ src/프로그래머스/lv1/추억점수.java
@@ -0,0 +1,29 @@
+package 프로그래머스.lv1;
+import java.util.*;
+
+public class 추억점수 {
+
+    class Solution {
+        public int[] solution(String[] name, int[] yearning, String[][] photo) {
+            int[] answer = new int[photo.length];
+
+            HashMap<String, Integer> memory = new HashMap<>();
+            for(int i = 0; i < name.length; i++){
+                memory.put(name[i], yearning[i]);
+            }
+
+            for(int i = 0; i < photo.length; i++){
+                int score = 0;
+                for(int j = 0; j < photo[i].length; j++){
+                    if (memory.containsKey(photo[i][j])){
+                        score += memory.get(photo[i][j]);
+                    }
+                }
+                answer[i] = score;
+            }
+
+
+            return answer;
+        }
+    }
+}
